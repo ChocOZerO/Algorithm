389673102a344a992d2a462312e298dc6b5847b4
ChocOZerO <nayalth@gmail.com>
2018-04-15 23:29:07 +0900

    [HackerRank]Sherlock and the Valid String

---
+++ HackerRank/Sherlock_and_the_Valid_String.playground/Contents.swift
@@ -0,0 +1,71 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+func isValid(input: String) -> Bool {
+    if input.count < 3 { return true }
+    let characterCounter = getCharacterCounter(input: input)
+    let counts = getCounts(from: characterCounter)
+    var differentCount = 0
+    var sameCounts = [Int: Int]()
+    for index in 1..<counts.count {
+        if counts[index] != counts[index-1] {
+            differentCount += 1
+            if sameCounts[counts[index-1]] == nil {
+                sameCounts[counts[index-1]] = 1
+            }
+            if sameCounts[counts[index]] == nil {
+                sameCounts[counts[index]] = 1
+            }
+        } else {
+            if sameCounts[counts[index]] == nil {
+                sameCounts[counts[index]] = 2
+            } else {
+                sameCounts[counts[index]]! += 1
+            }
+        }
+    }
+    if differentCount > 1 {
+        return false
+    } else if differentCount == 1 {
+        var keys = [Int]()
+        for key in sameCounts.keys {
+            keys.append(key)
+        }
+        keys.sort(by: <)
+        if keys.contains(1), sameCounts[1]! == 1 { return true }
+        if keys[1] == keys[0] + 1, sameCounts[keys[1]]! == 1 { return true }
+        return false
+    } else {
+        return true
+    }
+}
+
+func getCharacterCounter(input: String) -> [Character: Int] {
+    var characterCounter: [Character: Int] = [:]
+    for character in input {
+        if characterCounter[character] == nil {
+            characterCounter[character] = 1
+        } else {
+            characterCounter[character]! += 1
+        }
+    }
+    return characterCounter
+}
+
+func getCounts(from characterCounter: [Character: Int]) -> [Int] {
+    var counts = [Int]()
+    for character in characterCounter.keys {
+        counts.append(characterCounter[character]!)
+    }
+    counts.sort(by: <)
+    return counts
+}
+
+let tester = "aassdddd"
+let validation = isValid(input: tester)
+if validation {
+    print("YES")
+} else {
+    print("NO")
+}
